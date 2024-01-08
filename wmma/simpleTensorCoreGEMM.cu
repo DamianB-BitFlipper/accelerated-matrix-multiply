c1a80cb9c66faa3148275c62223abed1455e3eb4
Damian Barabonkov <damianb@alum.mit.edu>
2024-01-08 19:25:54 +0000

    Finished WMMA implementation

--- wmma/simpleTensorCoreGEMM.cu
+++ wmma/simpleTensorCoreGEMM.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdint>
 
 #include <curand.h>
@@ -22,12 +23,16 @@ const int32_t WMMA_K{ 16 };
 __global__ void wmma_matmul(
     half* a,
     half* b,
-    half* c,
+    half* bias,
+    half* c_out,
     int32_t M,
     int32_t N,
     int32_t K,
     float alpha,
     float beta) {
+    // Convert the `alpha` and `beta` weights into half precision
+    const half alpha_fp16{ __float2half(alpha) };
+    const half beta_fp16{ __float2half(beta) };
 
     // Tile using a 2D grid
     int32_t warpI = (blockIdx.x * blockDim.x + threadIdx.x) / WARP_SIZE;
@@ -36,14 +41,44 @@ __global__ void wmma_matmul(
     // Define the fragments
     wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> a_frag;
     wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> bias_frag;
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc_frag;
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> c_frag;
 
     wmma::fill_fragment(acc_frag, 0.0f);
+
+    const int32_t aRow{ WMMA_M * warpJ };
+    const int32_t bCol{ WMMA_N * warpI };
+
+    for (int32_t i{ 0 }; i < K; i += WMMA_K) {
+        const int32_t aCol{ i };
+        const int32_t bRow{ i };
+
+        // Bounds checking
+        if (aRow < M && bCol < N) {
+            // Fill the fragments noting the column-major memory format
+            wmma::load_matrix_sync(a_frag, a + aCol * M + aRow, M);
+            wmma::load_matrix_sync(b_frag, b + bCol * K + bRow, K);
+
+            // Perform the matrix multiplication
+            wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+        }
+    }
+
+    // Load and add in the bias
+    wmma::load_matrix_sync(bias_frag, bias + bCol * M + aRow, M, wmma::mem_col_major);
+
+#pragma unroll
+    for (int32_t i{ 0 }; i < acc_frag.num_elements; i++) {
+        c_frag.x[i] = alpha_fp16 * acc_frag.x[i] + beta_fp16 * bias_frag.x[i];
+    }
+
+    // Store the resulting output
+    wmma::store_matrix_sync(c_out + bCol * M + aRow, c_frag, M, wmma::mem_col_major);
 }
 
 __global__ void convertFp32ToFp16(float* in, half* out, const int32_t len) {
-    int32_t idx{ blockIdx.x * blockDim.x + threadIdx.x };
+    int32_t idx{ static_cast<int32_t>(blockIdx.x * blockDim.x + threadIdx.x) };
     if (idx < len) {
         out[idx] = static_cast<half>(in[idx]);
     }
@@ -58,6 +93,9 @@ int32_t main() {
     half* b_fp16;
     half* bias_fp16;
     half* c_fp16;
+
+    const float alpha{ 2.0f };
+    const float beta{ 2.0f };
 
     // Create and initialize the CUDA random number generator
     curandGenerator_t randGen;
@@ -117,11 +155,45 @@ int32_t main() {
         cudaStreamDestroy(stream3);
     }
 
+    // Some useful prints
+    std::cout << "M = " << MATRIX_M << ", ";
+    std::cout << "N = " << MATRIX_N << ", ";
+    std::cout << "K = " << MATRIX_K << ", ";
+    std::cout << std::fixed << std::setprecision(2) << "alpha = " << alpha << ", ";
+    std::cout << std::fixed << std::setprecision(2) << "beta = " << beta << std::endl;
+
+    std::cout << "Running with WMMA" << std::endl;
+
+    // Perform the WMMA matrix multiplication
+    {
+        dim3 blockDim{ 256, 4, 1 };
+        dim3 gridDim{
+            (MATRIX_N + (WMMA_N * blockDim.x / WARP_SIZE) - 1) / (WMMA_N * blockDim.x / WARP_SIZE),
+            (MATRIX_M + (WMMA_M * blockDim.y) - 1) / (WMMA_M * blockDim.y),
+            1 };
+
+        // Launch the WMMA matrix multiplication kernel
+        cudaEventRecord(startWMMA);
+        wmma_matmul<<<gridDim, blockDim>>>(
+            a_fp16,
+            b_fp16,
+            bias_fp16,
+            c_fp16,
+            MATRIX_M,
+            MATRIX_N,
+            MATRIX_K,
+            alpha,
+            beta);
+        cudaEventRecord(stopWMMA);
+        cudaEventSynchronize(stopWMMA);
+    }
+
     // Clean up
     curandDestroyGenerator(randGen);
     cudaEventDestroy(startWMMA);
     cudaEventDestroy(stopWMMA);
 
+    // Free all of the allocated memory
     cudaFree(a_fp32);
     cudaFree(b_fp32);
     cudaFree(bias_fp32);
