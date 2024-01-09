49722ed2549d144a55ba754c7e36f90e33fa7010
Damian Barabonkov <damianb@alum.mit.edu>
2024-01-09 14:06:50 +0000

    WMMA and CPU reference implementations agree

--- wmma/simpleTensorCoreGEMM.cu
+++ wmma/simpleTensorCoreGEMM.cu
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <memory>
 #include <algorithm>
+#include <chrono>
 
 #include <curand.h>
 #include <cublas_v2.h>
@@ -13,9 +14,9 @@ using namespace nvcuda;
 const int32_t WARP_SIZE{ 32 };
 
 // Must be multiples of 16 for wmma code to work
-const int32_t MATRIX_M{ 16384 };
-const int32_t MATRIX_N{ 16384 };
-const int32_t MATRIX_K{ 16384 };
+const int32_t MATRIX_M{ 1024 };
+const int32_t MATRIX_N{ 1024 };
+const int32_t MATRIX_K{ 1024 };
 
 // The only dimensions currently supported by WMMA
 const int32_t WMMA_M{ 16 };
@@ -86,6 +87,13 @@ __global__ void convertFp32ToFp16(float* in, half* out, const int32_t len) {
     }
 }
 
+__global__ void convertFp16ToFp32(half* in, float* out, const int32_t len) {
+    int32_t idx{ static_cast<int32_t>(blockIdx.x * blockDim.x + threadIdx.x) };
+    if (idx < len) {
+        out[idx] = static_cast<float>(in[idx]);
+    }
+}
+
 void referenceMatrixMultiply(
     float* a,
     float* b,
@@ -120,6 +128,7 @@ int32_t main() {
     float* a_fp32;
     float* b_fp32;
     float* bias_fp32;
+    float* c_fp32;
 
     half* a_fp16;
     half* b_fp16;
@@ -130,10 +139,21 @@ int32_t main() {
     cudaMalloc(&a_fp32, MATRIX_M * MATRIX_K * sizeof(float));
     cudaMalloc(&b_fp32, MATRIX_K * MATRIX_N * sizeof(float));
     cudaMalloc(&bias_fp32, MATRIX_M * MATRIX_N * sizeof(float));
+    cudaMalloc(&c_fp32, MATRIX_M * MATRIX_N * sizeof(float));
     cudaMalloc(&a_fp16, MATRIX_M * MATRIX_K * sizeof(half));
     cudaMalloc(&b_fp16, MATRIX_K * MATRIX_N * sizeof(half));
     cudaMalloc(&bias_fp16, MATRIX_M * MATRIX_N * sizeof(half));
     cudaMalloc(&c_fp16, MATRIX_M * MATRIX_N * sizeof(half));
+
+    std::unique_ptr<float[]> a_host{ new float[MATRIX_M * MATRIX_K * sizeof(float)] };
+    std::unique_ptr<float[]> b_host{ new float[MATRIX_K * MATRIX_N * sizeof(float)] };
+    std::unique_ptr<float[]> bias_host{ new float[MATRIX_M * MATRIX_N * sizeof(float)] };
+    std::unique_ptr<float[]> c_host{ new float[MATRIX_M * MATRIX_N * sizeof(float)] };
+    std::unique_ptr<float[]> c_wmma_host{ new float[MATRIX_M * MATRIX_N * sizeof(float)] };
+
+    // Clear the contents of `c` matrices
+    cudaMemset(c_fp16, __float2half(0.0f), MATRIX_M * MATRIX_N * sizeof(half));
+    std::fill(c_host.get(), c_host.get() + MATRIX_M * MATRIX_N, 0.0f);
 
     // Create and initialize the CUDA random number generator
     curandGenerator_t randGen;
@@ -145,14 +165,8 @@ int32_t main() {
     cudaEventCreate(&startWMMA);
     cudaEventCreate(&stopWMMA);
 
-    std::unique_ptr<float[]> a_host{ new float[MATRIX_M * MATRIX_K * sizeof(float)] };
-    std::unique_ptr<float[]> b_host{ new float[MATRIX_K * MATRIX_N * sizeof(float)] };
-    std::unique_ptr<float[]> bias_host{ new float[MATRIX_M * MATRIX_N * sizeof(float)] };
-    std::unique_ptr<float[]> c_host{ new float[MATRIX_M * MATRIX_N * sizeof(float)] };
-
-    // Clear the contents of `c` matrices
-    cudaMemset(c_fp16, __float2half(0.0f), MATRIX_M * MATRIX_N * sizeof(half));
-    std::fill(c_host.get(), c_host.get() + MATRIX_M * MATRIX_N, 0.0f);
+    // Create the timing variables for the reference CPU matrix multiply
+    std::chrono::high_resolution_clock::time_point referenceStartTime, referenceEndTime;
 
     // Curand does not support `half`, so generate random `float` and then convert to `half`
     {
@@ -199,11 +213,12 @@ int32_t main() {
         bias_host.get(), bias_fp32, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Some useful prints
-    std::cout << "M = " << MATRIX_M << ", ";
-    std::cout << "N = " << MATRIX_N << ", ";
-    std::cout << "K = " << MATRIX_K << ", ";
-    std::cout << std::fixed << std::setprecision(2) << "alpha = " << alpha << ", ";
-    std::cout << std::fixed << std::setprecision(2) << "beta = " << beta << std::endl;
+    std::cout << "M = " << MATRIX_M << ", "
+              << "N = " << MATRIX_N << ", "
+              << "K = " << MATRIX_K << ", "
+              << std::fixed << std::setprecision(2)
+              << "alpha = " << alpha << ", "
+              << "beta = " << beta << std::endl;
 
     std::cout << "Running with WMMA" << std::endl;
 
@@ -229,6 +244,70 @@ int32_t main() {
             beta);
         cudaEventRecord(stopWMMA);
         cudaEventSynchronize(stopWMMA);
+
+        // Convert the `c_fp16` to `c_fp32`
+        dim3 blockDimConvert{ 256, 1, 1 };
+        dim3 gridDimConvert{
+            (MATRIX_M * MATRIX_N + blockDimConvert.x - 1) / blockDimConvert.x, 1, 1 };
+
+        // Launch the kernel to convert the `c_fp16`
+        convertFp16ToFp32<<<gridDimConvert, blockDimConvert>>>(c_fp16, c_fp32, MATRIX_M * MATRIX_N);
+
+        // Copy the result to the `c_wmma_host`
+        cudaMemcpy(
+            c_wmma_host.get(), c_fp32, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToHost);
+    }
+
+    std::cout << "Running on CPU" << std::endl;
+
+    // Perform the CPU matrix multiplication
+    {
+        referenceStartTime = std::chrono::high_resolution_clock::now();
+        referenceMatrixMultiply(
+            a_host.get(),
+            b_host.get(),
+            bias_host.get(),
+            c_host.get(),
+            MATRIX_M,
+            MATRIX_N,
+            MATRIX_K,
+            alpha,
+            beta);
+        referenceEndTime = std::chrono::high_resolution_clock::now();
+    }
+
+    // Compare the matrix outputs
+    {
+        // Usa a 1% relative tolerance
+        int32_t errors = 0;
+        for (int32_t i = 0; i < MATRIX_M * MATRIX_N; i++) {
+            float v1 = c_host[i];
+            float v2 = c_wmma_host[i];
+            float diff  = fabs(v1 - v2);
+            float relative_err = diff / v2;
+            float eps = 0.01;
+            if (relative_err >= eps) {
+                errors++;
+                if (errors < 10) {
+                    std::cout << v1 << " " << v2 << std::endl;
+                }
+            }
+        }
+
+        if (errors > 0) {
+            std::cout << "WMMA does not agree with reference! " << errors << " errors!" << std::endl;
+        } else {
+            std::cout << "Results verified: reference and WMMA agree." << std::endl;
+            float wmmaTime;
+            cudaEventElapsedTime(&wmmaTime, startWMMA, stopWMMA);
+
+            std::chrono::milliseconds referenceDuration{
+                std::chrono::duration_cast<std::chrono::milliseconds>(
+                    referenceEndTime - referenceStartTime) };
+            std::cout << std::fixed << std::setprecision(4)
+                      << "wmma took " << wmmaTime << "ms\n"
+                      << "reference took " << referenceDuration.count() << "ms" << std::endl;
+        }
     }
 
     // Clean up
@@ -240,6 +319,7 @@ int32_t main() {
     cudaFree(a_fp32);
     cudaFree(b_fp32);
     cudaFree(bias_fp32);
+    cudaFree(c_fp32);
     cudaFree(a_fp16);
     cudaFree(b_fp16);
     cudaFree(bias_fp16);
